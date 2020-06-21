99b11245d3f12e9954988f2db36528c49f34bc71
Jay O'Conor <joconor@fastmail.fm>
2020-06-20 17:17:12 -0700

    Change the XAxis histogram labels

--- src/Histogram.js
+++ src/Histogram.js
@@ -14,15 +14,17 @@ import {
   ReferenceArea
 } from 'recharts';
 
-class Histogram extends Component{
+class Histogram extends Component {
 
   formatHistogram(histogram) {
+    const xAxisLabels = ["12mi", "1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a",
+      "12n", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p"]
     return Object.keys(histogram).sort().map(x => {
       return {
         // Display the histogram starting at midnight --local-- time.
         // To do this, access the histogram bins with the index adjusted by the timezone offset in hours.
-        'name': x,
-        'count': histogram[((parseInt(x) + new Date().getTimezoneOffset() / 60 + 24) % 24).toString().padStart(2,"0")]
+        'name': xAxisLabels[parseInt(x)],
+        'count': histogram[((parseInt(x) + new Date().getTimezoneOffset() / 60 + 24) % 24).toString().padStart(2, "0")]
       }
     })
   }
