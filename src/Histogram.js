3e7c7528f324af48c0181c297d3db4f99590ba47
Jay O'Conor <joconor@fastmail.fm>
2020-06-22 17:05:44 -0700

    Use an empty histogram if null histogram data is passed in

--- src/Histogram.js
+++ src/Histogram.js
@@ -14,21 +14,25 @@ import {
 class Histogram extends Component {
 
   formatHistogram(histogram) {
-    const xAxisLabels = ["12mi", "1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a",
-      "12n", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p"]
-    return Object.keys(histogram).sort().map(x => {
+    const emptyHistogram = [
+      { label: "12mi", value: 0 }, { label: "1a", value: 0 }, { label: "2a", value: 0 }, { label: "3a", value: 0 },
+      { label: "4a", value: 0 }, { label: "5a", value: 0 }, { label: "6a", value: 0 }, { label: "7a", value: 0 },
+      { label: "8a", value: 0 }, { label: "9a", value: 0 }, { label: "10a", value: 0 }, { label: "11a", value: 0 },
+      { label: "12n", value: 0 }, { label: "1p", value: 0 }, { label: "2p", value: 0 }, { label: "3p", value: 0 },
+      { label: "4p", value: 0 }, { label: "5p", value: 0 }, { label: "6p", value: 0 }, { label: "7p", value: 0 },
+      { label: "8p", value: 0 }, { label: "9p", value: 0 }, { label: "10p", value: 0 }, { label: "11p", value: 0 },
+    ];
+    // return Object.keys(histogram).sort().map(x => {
+      return [...Array(24).keys()].map(x => {
       return {
         // Display the histogram starting at midnight --local-- time.
         // To do this, access the histogram bins with the index adjusted by the timezone offset in hours.
-        'name': xAxisLabels[parseInt(x)],
-        'count': histogram[((parseInt(x) + new Date().getTimezoneOffset() / 60 + 24) % 24).toString().padStart(2, "0")]
+        'name': emptyHistogram[x].label,
+        'count': histogram === null ? emptyHistogram[x].value : histogram[((x + new Date().getTimezoneOffset() / 60 + 24) % 24).toString().padStart(2, "0")]
       }
     })
   }
   render() {
-    if (!this.props.histogram) {
-      return null;
-    }
     const bins = this.formatHistogram(this.props.histogram);
     return (<div>
       <ResponsiveContainer height={400}>
